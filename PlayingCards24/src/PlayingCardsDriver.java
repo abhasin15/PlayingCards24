5d6abfbbcc675ea6a02d96a42ba9f5b419930929
Luc Daniel <danilc@farmingdale.edu>
2019-04-04 19:41:31 -0400

    3rd commit

--- PlayingCards24/src/PlayingCardsDriver.java
+++ PlayingCards24/src/PlayingCardsDriver.java
@@ -8,6 +8,7 @@ public class PlayingCardsDriver extends Application {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello");
+		System.out.println("Hello Luc");
 		launch(args);
 	}
 
