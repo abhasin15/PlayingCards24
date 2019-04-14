2b30228bdba7aa54172491e7253a50778f4858d9
brena7 <48602872+brena7@users.noreply.github.com>
2019-04-14 15:59:44 -0400

    Adam, added cards class to handle images

--- PlayingCards24/src/PlayingCardsController.java
+++ PlayingCards24/src/PlayingCardsController.java
@@ -2,6 +2,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -23,5 +24,27 @@ public class PlayingCardsController {
 
 	}
 
+	// Adam added class to randomly pick cards
+	public class Cards extends PlayingCardsController {
+		
+		// Adam randomly select from 52 cards
+        int card = (int) (Math.floor(Math.random() * 51) + 1);
+		
+		// Adam display 4 cards using ImageView. use getImage method to draw cards from folder in project
+        public Image card1 = new ImageView("/PlayingCards24/src/cards" +
+		card + ".png").getImage();
+        
+        public Image card2 = new ImageView("/PlayingCards24/src/cards" +
+        		card + ".png").getImage();
+        
+        public Image card3 = new ImageView("/PlayingCards24/src/cards" +
+        		card + ".png").getImage();
+        
+        public Image card4 = new ImageView("/PlayingCards24/src/cards" +
+        		card + ".png").getImage();
+		
+		
+	}
+	
 
 }
