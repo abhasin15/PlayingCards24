8260471b570deec55a19e00eca9f72c7fcc738f9
Ayush Bhasin <bhasa@farmingdale.edu>
2019-04-25 21:50:14 -0400

    Different images load each time

--- PlayingCards24/src/PlayingCardsController.java
+++ PlayingCards24/src/PlayingCardsController.java
@@ -1,91 +1,48 @@
 
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+
+
+
+import java.net.URL;
 import java.util.Random;
-import java.util.Scanner;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 
 
-public class PlayingCardsController {
 
-
+public class PlayingCardsController implements Initializable{
 
 	static String rand = "";
 
 	@FXML
 	private Button solveTxt,clrTxt,chkTxt; //ayush
-
+	@FXML
 	private TextField ansField, entryField;
-
+	@FXML
 	private Label enterLbl;
-
+	@FXML 
 	private VBox vbox1,vbox2,vbox3,vbox4;
 
-
 	@FXML
-	// gather input from text field 
-	void input(KeyEvent event) {		
-		// to hold user input
-		String input;
-		// array for user input
-		char[] array;
-		int letter = 0;
-		int digit = 0;
-		int whitespace = 0;
-		// scanner to gather keyboard input
-		Scanner keyboard = new Scanner(System.in);
-		// set input equal to value gathered from scanner
-		input = keyboard.nextLine();
-
-		// convert string to char array
-		array = input.toCharArray();
-		// verify input is a valid digit
-		for (int i = 0; i < array.length; i++) {
-			if (Character.isLetter(array[i]))
-				letter--;
-			else if (Character.isWhitespace(array[i]))
-				whitespace--;	
-			else if (Character.isDigit(array[i]))
-				digit++;
-				entryField.setText(input);
-		}
+	protected ImageView card1;
+	@FXML
+	protected ImageView card2;
+	@FXML
+	protected ImageView card3;
+	@FXML
+	protected ImageView card4;
 	
-		// close scanner
-		keyboard.close();
-	}
-
-
-
 
 	@FXML
-	private ImageView card1;
-	@FXML
-	private ImageView card2;
-	@FXML
-	private ImageView card3;
-	@FXML
-	private ImageView card4;
-
-	public void startImgs() // this needs to work to show startup imgs
-	{
-		//int rando =(int) (Math.floor(Math.random() * 13) + 1);
-		card1.setImage(new ImageView("cards/11_of_diamonds.png").getImage());
-	}
-
-	@FXML
-	public static String imgFields()
+	public String imgFields()
 	{
 		String[] cardTypes = {"_of_clubs.png", "_of_diamonds.png", "_of_hearts.png", "_of_spades.png"};
 		Random r = new Random();
@@ -93,35 +50,58 @@ public class PlayingCardsController {
 		return rand;
 	}
 
-	//constructor to load random images at start of game
+	
 
 
 	@FXML
-	void loadImages(ActionEvent event) {
+	public void loadImages(ActionEvent event) {
 		ImageView fourCardStack[] = {card1,card2,card3,card4};
 		int[] cardRand = new int[4];
-		for(int counter = 0; counter < 4; counter ++)
-		{
-			cardRand[counter] = (int) (Math.floor(Math.random() * 13) + 1);
-			fourCardStack[counter].setImage(new ImageView("cards/"+cardRand[counter]+imgFields()).getImage());
-		}
-		/*	int card = (int) (Math.floor(Math.random() * 13) + 1);
-    	int cardTwo = (int) (Math.floor(Math.random() * 13) + 1);
-    	int cardThree = (int) (Math.floor(Math.random() * 13) + 1);
-    	int cardFour = (int) (Math.floor(Math.random() * 13) + 1);
-
-    	card1.setImage(new ImageView("cards/"+card+imgFields()).getImage());
-
-    	card2.setImage(new ImageView("cards/"+cardTwo+imgFields()).getImage());
-
-
-    	card3.setImage(new ImageView("cards/"+cardThree + imgFields()).getImage());
-
-    	card4.setImage(new ImageView("cards/"+cardFour + imgFields()).getImage());
-		 */
+			for(int counter = 0; counter < 4; counter ++)
+				{
+				cardRand[counter] = (int) (Math.floor(Math.random() * 13) + 1);
+				fourCardStack[counter].setImage(new ImageView("cards/"+cardRand[counter]+imgFields()).getImage());
+				}
+		
 	}
+	/*on key pressed input
+	action input
 
 
+
+		// gather input from text field 
+		void input(KeyEvent event) {		
+			// to hold user input
+			String input;
+			// array for user input
+			char[] array;
+			int letter = 0;
+			int digit = 0;
+			int whitespace = 0;
+			// scanner to gather keyboard input
+			Scanner keyboard = new Scanner(System.in);
+			// set input equal to value gathered from scanner
+			input = keyboard.nextLine();
+
+			// convert string to char array
+			array = input.toCharArray();
+			// verify input is a valid digit
+			for (int i = 0; i < array.length; i++) {
+				if (Character.isLetter(array[i]))
+					letter--;
+				else if (Character.isWhitespace(array[i]))
+					whitespace--;	
+				else if (Character.isDigit(array[i]))
+					digit++;
+					entryField.setText(input);
+			}
+		
+			// close scanner
+			keyboard.close();
+		}*/
+
+
+/*
 	// determine if operators entered are valid
 	public static boolean isOperator(char operator) {
 		return (operator == '(' ||
@@ -135,7 +115,7 @@ public class PlayingCardsController {
 				operator == '*');
 	}
 
-
+*/
 
 
 
@@ -145,5 +125,21 @@ public class PlayingCardsController {
 
 
 
-	}}
+	}
+
+
+
+
+@Override
+public void initialize(URL location, ResourceBundle resources) {
+	ImageView fourCardStack[] = {card1,card2,card3,card4};
+	int[] cardRand = new int[4];
+		for(int counter = 0; counter < 4; counter ++)
+			{
+			cardRand[counter] = (int) (Math.floor(Math.random() * 13) + 1);
+			fourCardStack[counter].setImage(new ImageView("cards/"+cardRand[counter]+imgFields()).getImage());
+			}
+	}
+	
+}
 
